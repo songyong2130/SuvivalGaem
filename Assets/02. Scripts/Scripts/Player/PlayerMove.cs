0884dd29ab7840744b79ab064f7c0e8ffd4bde69
songyong2130 <adqqqq1222@gmail.com>
2026-07-27 19:51:29 +0900

    적 로직 구현

--- Assets/02. Scripts/Scripts/Player/PlayerMove.cs
+++ Assets/02. Scripts/Scripts/Player/PlayerMove.cs
@@ -11,7 +11,7 @@ public class PlayerMove : MonoBehaviour
     [Header("플레이어 설정")]
     [SerializeField] private CharacterController cc_;
     
-    private float gravity = -16.8f;
+    private float gravity = -29.4f;
     private Vector2 moveInput;
     private Vector3 velocity;
     private bool isGrounded;
@@ -20,19 +20,29 @@ public class PlayerMove : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-        
+
+        // CharacterController가 비어있을시 CharacterController를 불러와 에러 방지
         if(cc_ == null) 
             cc_ = GetComponent<CharacterController>();
     }
     #region newInputSystem
+    /// <summary>
+    /// 플레이어의 움직임(WASD)를 감지하는 함수
+    /// </summary>
+    /// <param name="Move"></param>
     public void OnMove(InputAction.CallbackContext ctx)
     {
         moveInput = ctx.ReadValue<Vector2>();
     }
+    /// <summary>
+    /// 플레이어의 점프를 감지하는 함수(조건 있음)
+    /// </summary>
+    /// <param name="Jump"></param>
     public void OnJump(InputAction.CallbackContext ctx)
     {
         if (ctx.started && isGrounded)
         {
+            // Y축 최대지점까지의 속도값을 구하기 위해 jumpPower까지 도달하는 Yt
             velocity.y = Mathf.Sqrt(jumpPower * -2f * gravity);
         }
     }
@@ -40,13 +50,14 @@ public class PlayerMove : MonoBehaviour
     void Update()
     {
         isGrounded = cc_.isGrounded;
+        
         if (isGrounded && velocity.y < 0)
         {
-            velocity.y = -2f;
+            velocity.y = -2f; // 만약 경사로가 있을때 플레이어가 버벅거림을 방지하기 위해 지정함
         }
         
         Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
-        move.Normalize();
+        move.Normalize(); // 대각선 움직임이 더 빠른 현상 고침
         cc_.Move(move * moveSpeed* Time.deltaTime);
 
         velocity.y += gravity * Time.deltaTime;
--- Assets/02. Scripts/Scripts/Player/Sensitivity.cs
+++ Assets/02. Scripts/Scripts/Player/Sensitivity.cs
@@ -1,4 +1,3 @@
-using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,18 +7,28 @@ public class PlayerMouse : MonoBehaviour
     [SerializeField] private Transform playerCamera;
     
     [Header("인게임 설정")]
-    [SerializeField] private float mouseSensitivity = 15f;
+    [SerializeField] private float mouseSensitivity = 15f; 
     private float xRotation = 0f;
     private Vector2 lookInput;
     
+    /// <summary>
+    /// 플레이어의 마우스 움직임을 감지하는 함수
+    /// </summary>
+    /// <param name="Look"></param>
     public void OnLook(InputAction.CallbackContext ctx)
     {
-        if(!ctx.performed && !ctx.started) return;
-        lookInput = ctx.ReadValue<Vector2>();
+        
+        if (ctx.canceled)
+        {
+            lookInput = Vector2.zero ;
+            return;
+        }
+        if(ctx.performed) lookInput = ctx.ReadValue<Vector2>();
     }
-    
+    // 감도는 Update() 함수에서
     void Update()
     {
+        // 상하,좌우 마우스 각도 = 마우스 x,y좌표 움직임 * 마우스 감도 * 델타 타임
         float mouseX = lookInput.x * mouseSensitivity * Time.deltaTime;
         float mouseY = lookInput.y * mouseSensitivity * Time.deltaTime;
 
@@ -31,8 +40,7 @@ public class PlayerMouse : MonoBehaviour
 
         if (playerCamera != null)
         {
-        playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         }
-        lookInput = Vector2.zero;
     }
 }
